a2e6a7da8020497a0a307f151c27c6ff7365680d
argongs <gauravisvirtual@yahoo.com>
2018-03-11 11:52:17 +0530

    Forest Module Done

---
+++ Stock/server.R
@@ -0,0 +1,56 @@
+library('shiny')
+library('plotly')
+library('reshape')
+library('ggplot2')
+
+shinyServer(
+  function(input,output){
+  
+  #Forest Calculations - for stacked bargraph plot  
+  forest = read.csv("datasets/forest.csv", header=TRUE)
+  forest = data.frame(
+    Year = forest$Year,
+    Geographical_Area = forest$Geographical.Area,
+    Dense_Forests = forest$Very.Dense.Forest+forest$Moderately.Dense.Cover,
+    Open_Forests = forest$Open.Forest,
+    Scrub = forest$Non.Forest,
+    Non_Forest = forest$Non.Forest,
+    Mangrove = forest$Mangrove
+  )
+  morph = melt(forest, id.vars='Year')
+  
+  wld = read.csv("datasets/world_forest_cover.csv", header=FALSE)
+  w = unlist(wld[247,-1])
+  i = unlist(wld[105,-1])
+  comp = data.frame(Year=1990:2017, World=w, India=i)
+  comp_plot = ggplot(comp, aes(x=Year))+geom_line(aes(y=World), colour="red")+geom_line(aes(y=India), colour="green")
+  
+  tfc = read.csv("datasets/t10fc.csv", header=TRUE)
+  tfc.molten = melt(tfc, id.vars = 'Year')
+  observeEvent(input$year,{
+    toplot = tfc.molten[tfc.molten[,"Year"]==input$year,]
+    t10=ggplot(toplot, aes(x=variable, y=value))+geom_bar(stat = 'identity', colour = "green")
+    output$top10=renderPlot(t10)
+    }
+  )
+  
+    output$f_text = renderUI(
+      h5(
+        p("Forestry in India is a significant rural industry and a major environmental resource. India is one of the ten most forest-rich countries of the world along with Russia, Brazil, Canada, United States of America, China, Democratic Republic of the Congo, Australia, Indonesia and Sudan. 
+        Together, India and these countries account for 67 percent of total forest area of the world."),
+        p(" India's forest cover grew at 0.22 annually over 1990-2000, and has grown at the rate of 0.46% per year over 2000-2010, after decades where forest degradation was a matter of serious concern.")
+      )
+    )
+    
+  
+    #This will generate the plot of forest covers
+    output$forest = renderPlot(
+      ggplot(morph,aes(x=Year, y=value,fill=variable))+geom_bar(stat = 'identity')+
+      labs( x = "Year", y = "Area covered (in sq mi)", title = "Change in forest covers")
+    )
+    
+    output$compare = renderPlot(comp_plot+ylab("Year")+xlab("% Forest Cover"))
+  }
+ # forest=function(){}
+#  education=function(){}
+  )
---
+++ Stock/ui.R
@@ -0,0 +1,38 @@
+library('shiny')
+#Load Shiny Package
+
+#Define UI here
+shinyUI(fluidPage(
+  #Application Title
+  titlePanel("Indian Forest Cover Analysis"),
+  hr(),
+  #!Sidebar Layout begins here
+  sidebarLayout(
+    sidebarPanel(
+      
+      #Powered by
+      img(src="shiny.png", style="display: block; margin-left: auto; margin-right: auto"),
+      h4("A Shiny Creation", style="text-align:center"),
+      hr(),
+      
+      sliderInput(inputId = "year", label = "Year Transition", min = 2001, max = 2015, value = 0, step = 1, animate = TRUE),
+      #hr(),
+      
+      h5("Developed in ", style="text-align: center;"),
+      img(src="rstudio.png", style="display: block; margin-left: auto; margin-right: auto"),
+      br(),br()
+    ),
+    mainPanel(
+      tabsetPanel(
+        #tabPanel("Summary",icon = icon("list-alt"), uiOutput("summary")),
+        tabPanel("World Standing",icon = icon("bar-chart-o"), uiOutput("f_text"),plotOutput("top10")),#,
+        tabPanel("Forests Cover",icon = icon("bar-chart-o"),plotOutput("forest"), plotOutput("compare"))
+        #tabPanel("Table",icon = icon("table"), uiOutput("atable"),uiOutput("ctable"))
+      )
+      
+    )
+  )
+  
+  
+))
+
